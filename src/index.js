9ed408d5e9030be6d3579188fabb3740d9d63840
wallacesalima <wallacealmeida9999@gmail.com>
2022-08-18 19:19:44 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Arquivos SASS
+import './scss/index.scss'
+
+// Dependencias
+// Se não for colocado a extensão webpack entende que seja um arquivo .js
+import 'jquery'
+import 'bootstrap'
+
+// Meus arquivos JS
+import './js/core/includes'
+import './js/plugins/cityButtons'
